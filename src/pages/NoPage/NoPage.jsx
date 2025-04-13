4ee9efde8ab2efaa9c1e9d288a8c9a1186f0e4e0
Milan Sony <milansonyofficial@gmail.com>
2025-04-13 23:45:53 +0530

    style: no page created

---
+++ src/pages/NoPage/NoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NoPage() {
+    return (
+        <div className='w-full h-screen flex flex-col justify-center items-center'>
+            <h1 className='main-text text-5xl font-black'>404</h1>
+            <p className='font-mono text-xl'>Page not found !</p>
+        </div>
+    )
+}
+
+export default NoPage
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from "react-router";
 import IndexPage from '../pages/IndexPage/IndexPage';
 import SearchPage from '../pages/SearchPage/SearchPage';
+import NoPage from '../pages/NoPage/NoPage';
 
 function Router() {
     return (
         <Routes>
             <Route path='/' element={<IndexPage />} />
             <Route path='/search' element={<SearchPage />} />
+            <Route path='*' element={<NoPage />} />
         </Routes>
     )
 }
