cae7b90643d2935f8ba5db066c8d394aaed85d66
Milan Sony <milansonyofficial@gmail.com>
2025-04-13 23:08:29 +0530

    style: index page style modified

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router'
+
 import { Search } from 'lucide-react'
 import "./IndexPage.css"
 
@@ -7,12 +9,12 @@ function IndexPage() {
         <>
             <div className="hero bg-base-200 min-h-screen">
                 <div className="hero-content text-center">
-                    <div className="max-w-md">
+                    <div className="max-w-xl">
                         <h1 className="text-5xl font-bold main-text">MedGuide</h1>
                         <p className="py-6 main-text">
-                            An <span className="badge badge-soft badge-primary font-mono uppercase">AI-powered</span> medicine information application that provides users with detailed descriptions of medicines.
+                            An<span className="badge badge-soft badge-primary font-mono uppercase">AI-powered</span>app that offers detailed medicine descriptions💊
                         </p>
-                        <button className="btn btn-primary sub-text text-center"><Search color="white"/> Search</button>
+                        <Link to={"/search"} className='btn btn-primary sub-text'><span><Search color="white" /></span>Search</Link>
                     </div>
                 </div>
             </div>
